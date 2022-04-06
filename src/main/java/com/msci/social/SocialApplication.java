c8296545bff1a110faaac63ec5404172608178e3
rajpaas <aashoo.rajput@msci.com>
2022-04-07 00:03:12 +0530

    Update for external tomcat deploy

--- src/main/java/com/msci/social/SocialApplication.java
+++ src/main/java/com/msci/social/SocialApplication.java
@@ -2,12 +2,18 @@ package com.msci.social;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SocialApplication {
+public class SocialApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SocialApplication.class);
+	}
 }
